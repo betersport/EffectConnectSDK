8eb06156d0500926e58ba9a9be2961e3372ebc95
Carolien Rietdijk <carolien.rietdijk@betersport.nl>
2026-04-24 10:49:29 +0200

    Add line attribute + fix indent

--- EffectConnectSDK/Core/Model/Response/Line.php
+++ EffectConnectSDK/Core/Model/Response/Line.php
@@ -1,141 +1,147 @@
 <?php
-    namespace EffectConnect\PHPSdk\Core\Model\Response;
 
-    use EffectConnect\PHPSdk\Core\Helper\Payload;
+namespace EffectConnect\PHPSdk\Core\Model\Response;
+
+use EffectConnect\PHPSdk\Core\Helper\Payload;
+
+/**
+ * Class Line
+ *
+ * @author  Stefan Van den Heuvel
+ * @company Koek & Peer
+ * @product EffectConnect
+ * @package EffectConnectSDK
+ */
+final class Line
+{
+    /**
+     * @var LineIdentifiers $_identifiers
+     */
+    private $_identifiers;
+    /**
+     * @var int $_productId
+     */
+    private $_productId;
+    /**
+     * @var string $_productTitle
+     */
+    private $_productTitle;
+    /**
+     * @var float $_lineAmount
+     */
+    private $_lineAmount;
+    /**
+     * @var string $_status
+     */
+    private $_status;
+    /**
+     * @var LineStatusHistory[] $_statusHistory
+     */
+    private $_statusHistory = [];
+    /**
+     * @var LineFee[] $_fees
+     */
+    private $_fees = [];
+    /**
+     * @var OrderAttribute[] $_attributes
+     */
+    private $_attributes = [];
+    /**
+     * @var LineProductIdentifiers $_product
+     */
+    private $_product;
 
     /**
-     * Class Line
+     * Line constructor.
      *
-     * @author  Stefan Van den Heuvel
-     * @company Koek & Peer
-     * @product EffectConnect
-     * @package EffectConnectSDK
+     * @param $payload
      */
-    final class Line
+    public function __construct($payload)
     {
-        /**
-         * @var LineIdentifiers $_identifiers
-         */
-        private $_identifiers;
-        /**
-         * @var int $_productId
-         */
-        private $_productId;
-        /**
-         * @var string $_productTitle
-         */
-        private $_productTitle;
-        /**
-         * @var float $_lineAmount
-         */
-        private $_lineAmount;
-        /**
-         * @var string $_status
-         */
-        private $_status;
-        /**
-         * @var LineStatusHistory[] $_statusHistory
-         */
-        private $_statusHistory = [];
-        /**
-         * @var LineFee[] $_fees
-         */
-        private $_fees          = [];
-        /**
-         * @var LineProductIdentifiers $_product
-         */
-        private $_product;
-
-        /**
-         * Line constructor.
-         *
-         * @param $payload
-         */
-        public function __construct($payload)
-        {
-            $this->_identifiers   = new LineIdentifiers(Payload::extract($payload, 'identifiers'));
-            $this->_productId     = Payload::extract($payload, 'productId');
-            $this->_productTitle  = Payload::extract($payload, 'productTitle');
-            $this->_lineAmount    = (float)Payload::extract($payload, 'lineAmount');
-            $this->_status        = Payload::extract($payload, 'status');
-            $this->_product = new LineProductIdentifiers(Payload::extract($payload, 'product'));
-            if (Payload::contains($payload, 'statusHistory'))
-            {
-                foreach (Payload::extract($payload, 'statusHistory', true) as $historyNode)
-                {
-                    $this->_statusHistory[] = new LineStatusHistory($historyNode);
-                }
-            }
-            if (Payload::contains($payload, 'fees'))
-            {
-                foreach (Payload::extract($payload, 'fees', true) as $fee)
-                {
-                    $this->_fees[] = new LineFee($fee);
-                }
+        $this->_identifiers = new LineIdentifiers(Payload::extract($payload, 'identifiers'));
+        $this->_productId = Payload::extract($payload, 'productId');
+        $this->_productTitle = Payload::extract($payload, 'productTitle');
+        $this->_lineAmount = (float)Payload::extract($payload, 'lineAmount');
+        $this->_status = Payload::extract($payload, 'status');
+        $this->_product = new LineProductIdentifiers(Payload::extract($payload, 'product'));
+        if (Payload::contains($payload, 'statusHistory')) {
+            foreach (Payload::extract($payload, 'statusHistory', true) as $historyNode) {
+                $this->_statusHistory[] = new LineStatusHistory($historyNode);
             }
         }
-
-        /**
-         * @return LineIdentifiers
-         */
-        public function getIdentifiers()
-        {
-            return $this->_identifiers;
+        if (Payload::contains($payload, 'fees')) {
+            foreach (Payload::extract($payload, 'fees', true) as $fee) {
+                $this->_fees[] = new LineFee($fee);
+            }
         }
-
-        /**
-         * @return int
-         */
-        public function getProductId()
-        {
-            return $this->_productId;
-        }
-
-        /**
-         * @return string
-         */
-        public function getProductTitle()
-        {
-            return $this->_productTitle;
-        }
-
-        /**
-         * @return float
-         */
-        public function getLineAmount()
-        {
-            return $this->_lineAmount;
-        }
-
-        /**
-         * @return string
-         */
-        public function getStatus()
-        {
-            return $this->_status;
-        }
-
-        /**
-         * @return LineStatusHistory[]
-         */
-        public function getStatusHistory()
-        {
-            return $this->_statusHistory;
-        }
-
-        /**
-         * @return LineFee[]
-         */
-        public function getFees()
-        {
-            return $this->_fees;
-        }
-
-        /**
-         * @return LineProductIdentifiers
-         */
-        public function getProduct()
-        {
-            return $this->_product;
+        if (Payload::contains($payload, 'attributes')) {
+            foreach (Payload::extract($payload, 'attributes', true) as $attribute) {
+                $this->_attributes[] = new OrderAttribute($attribute);
+            }
         }
     }
+
+    /**
+     * @return LineIdentifiers
+     */
+    public function getIdentifiers()
+    {
+        return $this->_identifiers;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId()
+    {
+        return $this->_productId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProductTitle()
+    {
+        return $this->_productTitle;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLineAmount()
+    {
+        return $this->_lineAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->_status;
+    }
+
+    /**
+     * @return LineStatusHistory[]
+     */
+    public function getStatusHistory()
+    {
+        return $this->_statusHistory;
+    }
+
+    /**
+     * @return LineFee[]
+     */
+    public function getFees()
+    {
+        return $this->_fees;
+    }
+
+    /**
+     * @return LineProductIdentifiers
+     */
+    public function getProduct()
+    {
+        return $this->_product;
+    }
+}
